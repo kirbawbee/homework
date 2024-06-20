249c6b91f7315b701e5933f009dde1a7e9063ced
kirbawbee <charkasaukir@gmail.com>
2024-06-20 17:44:47 +0300

    initial commit

---
+++ example_test.go
@@ -0,0 +1,12 @@
+package hw01_test
+
+import (
+	"fmt"
+
+	"hw/hw01"
+)
+
+func ExampleSayHello() {
+	fmt.Println(hw01.SayHello())
+	// Output: привет, мир
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module hw/hw01
+
+go 1.22.2
